6a9aa522a43e7d4fb224dbc3211435b2cb8f695c
Kewin Dousse <me@protectator.ch>
2017-12-06 14:13:43 +0100

    Adding script to create DB

---
+++ create-db.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Mer 06 Décembre 2017 à 13:12
+-- Version du serveur :  5.7.20-0ubuntu0.17.04.1
+-- Version de PHP :  7.0.22-0ubuntu0.17.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `wdf`
+--
+CREATE DATABASE IF NOT EXISTS `wdf` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `wdf`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `computed_df`
+--
+
+CREATE TABLE `computed_df` (
+  `word` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `df` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `computed_tf`
+--
+
+CREATE TABLE `computed_tf` (
+  `url` varchar(1024) NOT NULL,
+  `word` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tf` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `computed_watch`
+--
+
+CREATE TABLE `computed_watch` (
+  `wdfId` varchar(64) NOT NULL,
+  `url` varchar(1024) NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `content`
+--
+
+CREATE TABLE `content` (
+  `id` int(11) NOT NULL,
+  `wdfId` varchar(64) NOT NULL,
+  `url` varchar(1024) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(8) DEFAULT NULL,
+  `title` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `content_text`
+--
+
+CREATE TABLE `content_text` (
+  `url` varchar(1024) NOT NULL,
+  `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `event`
+--
+
+CREATE TABLE `event` (
+  `wdfId` varchar(64) NOT NULL,
+  `url` varchar(512) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type` varchar(32) NOT NULL,
+  `value` varchar(512) DEFAULT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pagerequests`
+--
+
+CREATE TABLE `pagerequests` (
+  `wdfId` varchar(64) NOT NULL,
+  `url` varchar(1024) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `request` varchar(2048) NOT NULL,
+  `method` varchar(8) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pageviews`
+--
+
+CREATE TABLE `pageviews` (
+  `wdfId` varchar(64) NOT NULL,
+  `url` varchar(1024) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pagewatch`
+--
+
+CREATE TABLE `pagewatch` (
+  `wdfId` varchar(64) NOT NULL,
+  `url` varchar(512) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `amount` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `wdfId` int(11) NOT NULL,
+  `wdfToken` varchar(64) DEFAULT NULL,
+  `facebookId` bigint(20) NOT NULL,
+  `facebookAccessToken` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `computed_df`
+--
+ALTER TABLE `computed_df`
+  ADD PRIMARY KEY (`word`);
+
+--
+-- Index pour la table `computed_tf`
+--
+ALTER TABLE `computed_tf`
+  ADD PRIMARY KEY (`url`,`word`);
+
+--
+-- Index pour la table `computed_watch`
+--
+ALTER TABLE `computed_watch`
+  ADD PRIMARY KEY (`wdfId`,`url`);
+
+--
+-- Index pour la table `content`
+--
+ALTER TABLE `content`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `content_text`
+--
+ALTER TABLE `content_text`
+  ADD PRIMARY KEY (`url`);
+
+--
+-- Index pour la table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `pagerequests`
+--
+ALTER TABLE `pagerequests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `pageviews`
+--
+ALTER TABLE `pageviews`
+  ADD PRIMARY KEY (`wdfId`,`url`,`timestamp`);
+
+--
+-- Index pour la table `pagewatch`
+--
+ALTER TABLE `pagewatch`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`wdfId`),
+  ADD UNIQUE KEY `facebookId` (`facebookId`),
+  ADD KEY `wdfIf` (`wdfId`),
+  ADD KEY `facebookId_2` (`facebookId`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `content`
+--
+ALTER TABLE `content`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT pour la table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `pagerequests`
+--
+ALTER TABLE `pagerequests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1449;
+--
+-- AUTO_INCREMENT pour la table `pagewatch`
+--
+ALTER TABLE `pagewatch`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `wdfId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
